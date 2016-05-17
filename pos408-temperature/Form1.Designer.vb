f5d2851610d54dcb4af21015e9e4b0d0a8c96c85
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-05-16 23:05:28 -0700

    replace textboxes with labels and add error message

--- pos408-temperature/Form1.Designer.vb
+++ pos408-temperature/Form1.Designer.vb
@@ -22,12 +22,15 @@ Partial Class TemperatureConversion
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.cmbUnit1 = New System.Windows.Forms.ComboBox()
         Me.txtTemp1 = New System.Windows.Forms.TextBox()
         Me.btnConvert = New System.Windows.Forms.Button()
         Me.lblTitle = New System.Windows.Forms.Label()
-        Me.txtTemp2 = New System.Windows.Forms.TextBox()
-        Me.txtUnit2 = New System.Windows.Forms.TextBox()
+        Me.lblError = New System.Windows.Forms.Label()
+        Me.lblTemp2 = New System.Windows.Forms.Label()
+        Me.lblUnit2 = New System.Windows.Forms.Label()
+        Me.tmrError = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'cmbUnit1
@@ -39,7 +42,7 @@ Partial Class TemperatureConversion
         Me.cmbUnit1.Location = New System.Drawing.Point(131, 63)
         Me.cmbUnit1.Name = "cmbUnit1"
         Me.cmbUnit1.Size = New System.Drawing.Size(41, 28)
-        Me.cmbUnit1.TabIndex = 0
+        Me.cmbUnit1.TabIndex = 1
         '
         'txtTemp1
         '
@@ -47,7 +50,7 @@ Partial Class TemperatureConversion
         Me.txtTemp1.Location = New System.Drawing.Point(18, 63)
         Me.txtTemp1.Name = "txtTemp1"
         Me.txtTemp1.Size = New System.Drawing.Size(107, 26)
-        Me.txtTemp1.TabIndex = 1
+        Me.txtTemp1.TabIndex = 0
         '
         'btnConvert
         '
@@ -69,31 +72,49 @@ Partial Class TemperatureConversion
         Me.lblTitle.TabIndex = 7
         Me.lblTitle.Text = "Temperature Conversion"
         '
-        'txtTemp2
+        'lblError
         '
-        Me.txtTemp2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtTemp2.Location = New System.Drawing.Point(185, 63)
-        Me.txtTemp2.Name = "txtTemp2"
-        Me.txtTemp2.ReadOnly = True
-        Me.txtTemp2.Size = New System.Drawing.Size(108, 26)
-        Me.txtTemp2.TabIndex = 8
+        Me.lblError.AutoSize = True
+        Me.lblError.BackColor = System.Drawing.Color.Black
+        Me.lblError.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblError.ForeColor = System.Drawing.Color.Red
+        Me.lblError.Location = New System.Drawing.Point(15, 182)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(0, 16)
+        Me.lblError.TabIndex = 10
         '
-        'txtUnit2
+        'lblTemp2
         '
-        Me.txtUnit2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtUnit2.Location = New System.Drawing.Point(299, 63)
-        Me.txtUnit2.Name = "txtUnit2"
-        Me.txtUnit2.ReadOnly = True
-        Me.txtUnit2.Size = New System.Drawing.Size(34, 26)
-        Me.txtUnit2.TabIndex = 9
+        Me.lblTemp2.AutoSize = True
+        Me.lblTemp2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTemp2.Location = New System.Drawing.Point(181, 66)
+        Me.lblTemp2.Name = "lblTemp2"
+        Me.lblTemp2.Size = New System.Drawing.Size(57, 20)
+        Me.lblTemp2.TabIndex = 11
+        Me.lblTemp2.Text = "Label2"
+        '
+        'lblUnit2
+        '
+        Me.lblUnit2.AutoSize = True
+        Me.lblUnit2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUnit2.Location = New System.Drawing.Point(313, 66)
+        Me.lblUnit2.Name = "lblUnit2"
+        Me.lblUnit2.Size = New System.Drawing.Size(20, 20)
+        Me.lblUnit2.TabIndex = 12
+        Me.lblUnit2.Text = "C"
+        '
+        'tmrError
+        '
+        Me.tmrError.Interval = 3000
         '
         'TemperatureConversion
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(361, 185)
-        Me.Controls.Add(Me.txtUnit2)
-        Me.Controls.Add(Me.txtTemp2)
+        Me.ClientSize = New System.Drawing.Size(361, 218)
+        Me.Controls.Add(Me.lblUnit2)
+        Me.Controls.Add(Me.lblTemp2)
+        Me.Controls.Add(Me.lblError)
         Me.Controls.Add(Me.lblTitle)
         Me.Controls.Add(Me.btnConvert)
         Me.Controls.Add(Me.txtTemp1)
@@ -108,7 +129,9 @@ Partial Class TemperatureConversion
     Friend WithEvents txtTemp1 As System.Windows.Forms.TextBox
     Friend WithEvents btnConvert As System.Windows.Forms.Button
     Friend WithEvents lblTitle As System.Windows.Forms.Label
-    Friend WithEvents txtTemp2 As System.Windows.Forms.TextBox
-    Friend WithEvents txtUnit2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblError As System.Windows.Forms.Label
+    Friend WithEvents lblTemp2 As System.Windows.Forms.Label
+    Friend WithEvents lblUnit2 As System.Windows.Forms.Label
+    Friend WithEvents tmrError As System.Windows.Forms.Timer
 
 End Class
--- pos408-temperature/Form1.vb
+++ pos408-temperature/Form1.vb
@@ -1,29 +1,50 @@
 ﻿Public Class TemperatureConversion
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         cmbUnit1.SelectedIndex = 1
-        txtUnit2.Text = "C"
+        lblUnit2.Text = "C"
     End Sub
 
     Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
         Dim temperature As Double
         If Double.TryParse(txtTemp1.Text, temperature) Then
             If cmbUnit1.SelectedIndex = 0 Then
-                txtTemp2.Text = (temperature * 9 / 5 + 32).ToString()
+                lblTemp2.Text = (temperature * 9 / 5 + 32).ToString()
             Else
-                txtTemp2.Text = ((temperature - 32) * 5 / 9).ToString()
+                lblTemp2.Text = ((temperature - 32) * 5 / 9).ToString()
             End If
         Else
-            txtTemp1.Text = ""
-            txtTemp2.Text = ""
+            txtTemp1.ForeColor = Color.Red
+            txtTemp1.BackColor = Color.Black
+            lblError.Text = "Temperature not valid" + vbNewLine
+            tmrError.Enabled = True
         End If
     End Sub
 
     Private Sub cmbUnit1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbUnit1.SelectedIndexChanged
-        txtTemp2.Text = ""
+        lblTemp2.Text = ""
         If cmbUnit1.SelectedIndex = 0 Then
-            txtUnit2.Text = "F"
+            lblUnit2.Text = "F"
         Else
-            txtUnit2.Text = "C"
+            lblUnit2.Text = "C"
         End If
+    End Sub
+
+    Private Sub tmrError_Tick(sender As Object, e As EventArgs) Handles tmrError.Tick
+        ClearError()
+    End Sub
+
+    Private Sub txtTemp1_Enter(sender As Object, e As EventArgs) Handles txtTemp1.Enter
+        ClearError()
+    End Sub
+
+    Private Sub cmbUnit1_Enter(sender As Object, e As EventArgs) Handles cmbUnit1.Enter
+        ClearError()
+    End Sub
+
+    Private Sub ClearError()
+        txtTemp1.ForeColor = Color.Black
+        txtTemp1.BackColor = Color.White
+        lblError.Text = ""
+        tmrError.Enabled = False
     End Sub
 End Class
